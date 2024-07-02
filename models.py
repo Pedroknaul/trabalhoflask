526d522d6e62c11253283ee0d31c31987415c2eb
Pedroknaul <164891191+Pedroknaul@users.noreply.github.com>
2024-07-02 12:23:02 -0300

    models.py

--- models.py
+++ models.py
@@ -1,5 +1,3 @@
-# models.py
-
 from pony.orm import Database, Required, set_sql_debug
 
 db = Database()
@@ -9,9 +7,7 @@ class Roupa(db.Entity):
     categoria = Required(str)
     tamanho = Required(str)
 
-# Conecte-se ao banco de dados SQLite
 db.bind(provider='sqlite', filename='bdroupa.sqlite', create_db=True)
 
-# Crie as tabelas
 db.generate_mapping(create_tables=True)
 set_sql_debug(True)
